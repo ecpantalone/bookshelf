e9588f9730dc516654b2f4607d63b0048fbc8bbe
ecpantalone <ecpantalone@gmail.com>
2020-06-24 14:27:09 -0400

    establishing initialization, addng blueprints

---
+++ __init__.py
@@ -0,0 +1,26 @@
+import os
+from flask import Flask__
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        SQLALCHEMY_DATABASE_URI = 'sqlite:///flasklibrary.db',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    db.init_app(app)
+
+    # blueprint for auth routes in our app
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    # blueprint for non-auth parts of app
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
