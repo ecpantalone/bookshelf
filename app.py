85e1b150a889e11fd71351149cb7f97e67b770b2
ecpantalone <ecpantalone@gmail.com>
2020-06-24 17:38:35 -0400

    why not rename the main app file a third time?

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Blueprint('app', __name__)
 
-db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -24,6 +23,3 @@ def profile():
 # @app.route('/logout')
 # def logout():
 #     return render_template('logout.html')    
-
-if __name__ == "__main__":
-    app.run(debug=True)
