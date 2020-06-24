d74a1e3f28753afe18b2fafbc1a04f26ab2ed57b
ecpantalone <ecpantalone@gmail.com>
2020-06-24 15:53:07 -0400

    add routes

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy 
+from flask import Flask, render_template, Blueprint
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flasklibrary.db'
 db = SQLAlchemy(app)
-
-# class User(db.Model):
-#     email_address = db.Column(db.String(250), primary_key=True)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/logout')
+def logout():
+    return render_template('logout.html')    
 
 if __name__ == "__main__":
     app.run(debug=True)
