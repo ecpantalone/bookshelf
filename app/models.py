89020bab5e96dc833dc12217d92f9705127d5fcb
ecpantalone <ecpantalone@gmail.com>
2020-06-25 20:04:41 -0400

    the model so far

---
+++ app/models.py
@@ -0,0 +1,43 @@
+from app import db
+from datetime import datetime
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), index=True, unique=True)
+    email = db.Column(db.String(200), index=True, unique=True)
+    password_hash = db.Column(db.String(200))
+    books = db.Column(db.Integer, db.ForeignKey('book.id'))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(1000), index=True, unique=False)
+    author = db.Column(db.String(1000), index=True, unique=False)
+    notes = db.Column(db.String(1500))
+    purchase_date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Book {}>'.format(self.title)
+
+# I could see a usecase for having an Author table alongside a Book table and a join table for holding 
+# all the books on a user's shelf (maybe call it Bookshelf), but ultimately I am choosing a simple db 
+# approach, focusing more on function of the app itself. I would have gone the other way if I were building
+# an app that was a catalog of books, where we didn't want/need duplicates.
+
+# class Author(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     first_name = db.Column(db.String(1000), index=True, unique=False)
+#     last_name = db.Column(db.String(1000), index=True, uniqe=False)
+
+#     def __repr__(self):
+#         return '<Author {}>'.format(self.last_name)
+
+# class Bookshelf(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+#     user_id = db.Column(db.Integer, db.ForeignKey('user.username'))
+
+#     def __repr__(self):
+#         return '<Bookshelf {}>'.formate(self.user_id)
